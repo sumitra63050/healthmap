5ea885618dec58c3571679a9a872b7e7715d2bd3
sumitra63050 <sumitrachoudhary63050@gmail.com>
2026-03-30 19:21:48 +0530

    Switch to middleware for final Express 5 fix

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI, {
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-    app.get("/:path*", (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
     })
 } else {
